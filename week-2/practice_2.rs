32ea23dbbcfeb885bf90a1b01f03a930655efd29
Olukayode Ogunmoroti <olukayode.ogunmoroti@pau.edu.ng>
2025-10-09 15:55:03 +0100

    Uploaded practice 1-4

---
+++ week-2/practice_2.rs
@@ -0,0 +1,9 @@
+fn main(){
+	println!(); //print just a
+
+	println!("hello"); //prints hello
+
+	println!("format {} arguments,", "some"); // prints format some arguments
+
+	println!("My name is {}. I am the class rep of Data Science", "Olukayode Ogunmoroti");
+}
