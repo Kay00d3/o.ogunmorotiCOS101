0a7ccb11644fe0fa45ae0113d379992436a57f2c
Olukayode Ogunmoroti <olukayode.ogunmoroti@pau.edu.ng>
2025-11-05 13:24:06 +0100

    Uploaded practice-8

---
+++ week-6/practice-8/target/debug/deps/practice_8.d
@@ -0,0 +1,5 @@
+C:\Users\WALE\Desktop\o.ogunmorotiCOS101\week-6\practice-8\target\debug\deps\practice_8.d: src\main.rs
+
+C:\Users\WALE\Desktop\o.ogunmorotiCOS101\week-6\practice-8\target\debug\deps\practice_8.exe: src\main.rs
+
+src\main.rs:
---
+++ week-6/practice-8/target/debug/practice-8.d
@@ -0,0 +1,1 @@
+C:\Users\WALE\Desktop\o.ogunmorotiCOS101\week-6\practice-8\target\debug\practice-8.exe: C:\Users\WALE\Desktop\o.ogunmorotiCOS101\week-6\practice-8\src\main.rs
