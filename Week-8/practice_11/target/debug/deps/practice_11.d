82dd087e39b1bad639b0febe29f4f040f078ae1e
Olukayode Ogunmoroti <olukayode.ogunmoroti@pau.edu.ng>
2025-11-19 13:37:53 +0100

    Using rust slice

---
+++ Week-8/practice_11/target/debug/deps/practice_11.d
@@ -0,0 +1,5 @@
+C:\Users\WALE\Desktop\o.ogunmorotiCOS101\Week-8\practice_11\target\debug\deps\practice_11.d: src\main.rs
+
+C:\Users\WALE\Desktop\o.ogunmorotiCOS101\Week-8\practice_11\target\debug\deps\practice_11.exe: src\main.rs
+
+src\main.rs:
---
+++ Week-8/practice_11/target/debug/practice_11.d
@@ -0,0 +1,1 @@
+C:\Users\WALE\Desktop\o.ogunmorotiCOS101\Week-8\practice_11\target\debug\practice_11.exe: C:\Users\WALE\Desktop\o.ogunmorotiCOS101\Week-8\practice_11\src\main.rs
