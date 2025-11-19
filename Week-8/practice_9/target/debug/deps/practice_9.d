d626c73789937d62475392dd9ebe0c09b8d1c3a9
Olukayode Ogunmoroti <olukayode.ogunmoroti@pau.edu.ng>
2025-11-19 13:31:24 +0100

    Passing a tuple as a parameter

---
+++ Week-8/practice_9/target/debug/deps/practice_9.d
@@ -0,0 +1,5 @@
+C:\Users\WALE\Desktop\o.ogunmorotiCOS101\Week-8\practice_9\target\debug\deps\practice_9.d: src\main.rs
+
+C:\Users\WALE\Desktop\o.ogunmorotiCOS101\Week-8\practice_9\target\debug\deps\practice_9.exe: src\main.rs
+
+src\main.rs:
---
+++ Week-8/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+C:\Users\WALE\Desktop\o.ogunmorotiCOS101\Week-8\practice_9\target\debug\practice_9.exe: C:\Users\WALE\Desktop\o.ogunmorotiCOS101\Week-8\practice_9\src\main.rs
