5412d6b48a81b9a68e3d54ab4ed21a9eb4aa0848
Olukayode Ogunmoroti <olukayode.ogunmoroti@pau.edu.ng>
2025-11-26 12:33:10 +0100

    Deleted a file using rust

---
+++ week-9/practice_3/target/debug/deps/practice_3.d
@@ -0,0 +1,5 @@
+C:\Users\WALE\Desktop\o.ogunmorotiCOS101\week-9\practice_3\target\debug\deps\practice_3.d: src\main.rs
+
+C:\Users\WALE\Desktop\o.ogunmorotiCOS101\week-9\practice_3\target\debug\deps\practice_3.exe: src\main.rs
+
+src\main.rs:
