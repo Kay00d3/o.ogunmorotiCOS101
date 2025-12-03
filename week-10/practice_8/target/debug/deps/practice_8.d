78c3436fbc3ce71a96ad63b135334d2c15c768e9
Olukayode Ogunmoroti <olukayode.ogunmoroti@pau.edu.ng>
2025-12-03 13:46:12 +0100

    Passing a struct to a function

---
+++ week-10/practice_8/target/debug/deps/practice_8.d
@@ -0,0 +1,5 @@
+C:\Users\WALE\Desktop\o.ogunmorotiCOS101\week-10\practice_8\target\debug\deps\practice_8.d: src\main.rs
+
+C:\Users\WALE\Desktop\o.ogunmorotiCOS101\week-10\practice_8\target\debug\deps\practice_8.exe: src\main.rs
+
+src\main.rs:
